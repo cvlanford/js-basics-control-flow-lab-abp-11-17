f476e5af9a6e856e412e773999fe883db185bd6c
Caroline Lanford <caroline.lanford@gmail.com>
2018-02-02 20:46:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ function scuberGreetingForFeet(feet){
 }
 
 function ternaryCheckCity(city){
-  if (city == NYC){
-    return `Ok, sounds good.`;
-  } else {
-    return`No go.`;
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
   }
 }
